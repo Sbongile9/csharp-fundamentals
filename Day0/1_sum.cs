89cf3054bfc12db4bd9c171678b0c7cfe27274b0
Sbongile9 <enhle341@gmail.com>
2026-08-03 11:27:02 +0200

    sum

---
+++ Day0/1_sum.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+
+        int sum = a + b;
+        Console.Write(sum);
+    }
+}
